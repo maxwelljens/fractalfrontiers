7479602a14eefb570a626ef13593626732c005e2
maxwelljens <85795372+maxwelljens@users.noreply.github.com>
2024-07-11 15:24:26 +0200

    test

--- ship.gd
+++ ship.gd
@@ -1,8 +1,21 @@
 extends CharacterBody2D
 
+const TOP_SPEED = 500
+const SPEED_MULTIPLIER = 10
+const DRAG = 5
+
 func _physics_process(delta):
-  # if is_multiplayer_authority():
-  velocity = Input.get_vector("left", "right", "forward", "backward") * 400
+  if is_multiplayer_authority():
+    if Input.is_action_pressed("forward"):
+      velocity += Vector2.RIGHT.rotated(rotation) * SPEED_MULTIPLIER
+    if Input.is_action_pressed("backward"):
+      velocity += Vector2.RIGHT.rotated(rotation + PI) * SPEED_MULTIPLIER
+    if Input.is_action_pressed("right"):
+      rotation += 0.05
+    if Input.is_action_pressed("left"):
+      rotation += -0.05
+  velocity = velocity.move_toward(Vector2(0, 0), DRAG)
+  velocity = velocity.clamp(Vector2(-TOP_SPEED, -TOP_SPEED), Vector2(TOP_SPEED, TOP_SPEED))
   move_and_slide()
 
 func _enter_tree():
