847650b9942e7f95fb44c1638b78aa5315151da6
ivankorchmit <criperadmiral92@gmail.com>
2024-07-13 17:59:09 +0300

    fix laser

--- player.gd
+++ player.gd
@@ -16,8 +16,7 @@ func _ready():
 
 func _physics_process(_delta):
   if Input.is_action_pressed("ui_accept") and targets != null:
-    $Line2D.add_point(global_position)
-    $Line2D.add_point(TargetHolder.instance.targets[0].global_position)
+    _draw_line()
   if Input.is_action_just_released("ui_accept"):
     $Line2D.clear_points()
   if targets != null:
@@ -26,3 +25,14 @@ func _physics_process(_delta):
 
 func _enter_tree():
   set_multiplayer_authority(name.to_int())
+
+
+func _draw_line() -> void:
+    if TargetHolder.instance.targets.size() == 0: return
+    
+    var line := $Line2D as Line2D
+    
+    if line.get_point_count() < 2:
+        line.add_point(Vector2.ZERO)
+        line.add_point(to_local(TargetHolder.instance.targets[0].global_position))
+    line.set_point_position(1, to_local(TargetHolder.instance.targets[0].global_position))
