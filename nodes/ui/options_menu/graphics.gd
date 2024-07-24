a64479cfdddc7510f701ee2df386dc09785f91a0
maxwelljens <85795372+maxwelljens@users.noreply.github.com>
2024-07-24 16:33:32 +0200

    master

--- nodes/ui/options_menu/graphics.gd
+++ nodes/ui/options_menu/graphics.gd
@@ -8,6 +8,8 @@ const SCREEN_MODES: Array[StringName] = [
 ]
 
 const RESOLUTIONS: Array[Vector2i] = [
+  Vector2i(800, 600),
+  Vector2i(1024, 600),
   Vector2i(1152, 648),
   Vector2i(1280, 720),
   Vector2i(1440, 900),
@@ -19,36 +21,41 @@ const RESOLUTIONS: Array[Vector2i] = [
 
 @onready var screen_mode_ob: OptionButton = %ScreenModeOB
 @onready var resolution_ob: OptionButton = %ResolutionOB
+@onready var scale_slider: HSlider = %ScaleSlider
+@onready var scale_percentage: Label = %ScalePercentage
+@onready var scale_apply_button: Button = %ScaleApplyButton
 
 func _ready() -> void:
   screen_mode_ob.item_selected.connect(_on_screen_mode_selected)
   resolution_ob.item_selected.connect(_update_resolution_options)
+  scale_slider.value_changed.connect(_on_scale_slider_value_changed)
+  scale_apply_button.pressed.connect(_on_scale_apply_button_pressed)
   _populate_screen_mode_options()
   _update_screen_mode_selection()
   _populate_resolution_options()
   _update_resolution_options()
 
 func _process(_delta) -> void:
-  # Disable resolution option button if windowed
-  if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
-    resolution_ob.disabled = true
-  else:
-    resolution_ob.disabled = false
+  pass
 
 func _on_screen_mode_selected(index: int) -> void:
   match index:
     0: # Fullscreen
       DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
       DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+      resolution_ob.disabled = true
     1: # Windowed
       DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
       DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+      resolution_ob.disabled = false
     2: # Borderless Windowed
       DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
       DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+      resolution_ob.disabled = false
     3: # Borderless Fullscreen 
       DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
       DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+      resolution_ob.disabled = true
 
 func _populate_screen_mode_options() -> void:
   for x in SCREEN_MODES:
@@ -67,13 +74,15 @@ func _update_screen_mode_selection() -> void:
       screen_mode_ob.select(1)
 
 func _populate_resolution_options() -> void:
-  for x in RESOLUTIONS:
-    var resolution_string := "%s x %s" % [x.x, x.y]
+  for vec in RESOLUTIONS:
+    var resolution_string := "%s x %s" % [vec.x, vec.y]
     resolution_ob.add_item(resolution_string)
 
 func _update_resolution_options() -> void:
   var screen_size: Vector2i = DisplayServer.screen_get_size()
-  for res in RESOLUTIONS:
-    if res == screen_size:
-      resolution_ob.select(RESOLUTIONS.find(res))
-      DisplayServer.window_set_size(res)
+
+func _on_scale_slider_value_changed(_value: float) -> void:
+  scale_percentage.text = "%s%%" % [scale_slider.value * 100]
+
+func _on_scale_apply_button_pressed() -> void:
+  get_window().content_scale_factor = scale_slider.value
