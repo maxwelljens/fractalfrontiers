aa906ba01b76f161da56873ac71ab0d6eacc75c3
Maxwell Jensen <85795372+maxwelljens@users.noreply.github.com>
2025-03-15 23:07:30 +0100

    Commit

--- nodes/ui/cockpit_view.gd
+++ nodes/ui/cockpit_view.gd
@@ -11,18 +11,16 @@ var dragging = false
 var mouse_start_x = 0
 var progress_start = 0
 
-
-
 func _input(event):
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT:
-            if event.pressed:
-                dragging = true
-                mouse_start_x = event.position.x
-                progress_start = stick.progress_ratio
-            else:
-                dragging = false
+  if event is InputEventMouseButton:
+    if event.button_index == MOUSE_BUTTON_LEFT:
+      if event.pressed:
+        dragging = true
+        mouse_start_x = event.position.x
+        progress_start = stick.progress_ratio
+      else:
+        dragging = false
     
-    if event is InputEventMouseMotion and dragging:
-        var delta = event.position.x - mouse_start_x
-        stick.progress_ratio = clamp(progress_start + delta * sensitivity, 0.0, 1.0)
+  if event is InputEventMouseMotion and dragging:
+    var delta = event.position.x - mouse_start_x
+    stick.progress_ratio = clamp(progress_start + delta * sensitivity, 0.0, 1.0)
