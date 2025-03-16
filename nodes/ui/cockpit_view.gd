cab880201d215be3fe2d198ec95bc87299934c5e
Maxwell Jensen <85795372+maxwelljens@users.noreply.github.com>
2025-03-16 18:09:29 +0100

    Commit

--- nodes/ui/cockpit_view.gd
+++ nodes/ui/cockpit_view.gd
@@ -2,7 +2,7 @@ class_name CockpitView extends CanvasLayer
 
 @export var stick: PathFollow3D
 @export var end_stick: Node3D
-@export var sensitivity = 0.02
+@export var sensitivity = 0.01
 
 var stick_moving: bool
 var stick_move_position: Vector3
@@ -11,21 +11,18 @@ var dragging = false
 var mouse_start_x = 0
 var progress_start = 0
 
-
 func _input(event: InputEvent) -> void:
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
-            dragging = false
-    if event is InputEventMouseMotion and dragging:
-        var delta = event.position.x - mouse_start_x
-        stick.progress_ratio = clamp(progress_start + delta * sensitivity, 0.0, 1.0)
-
+  if event is InputEventMouseButton:
+    if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+      dragging = false
+  if event is InputEventMouseMotion and dragging:
+    var delta = -event.position.y - mouse_start_x
+    stick.progress_ratio = clamp(progress_start + delta * sensitivity, 0.0, 1.0)
 
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
   if event is InputEventMouseButton:
     if event.button_index == MOUSE_BUTTON_LEFT:
       if event.pressed:
         dragging = true
-        print(event_position)
-        mouse_start_x = event.position.x
+        mouse_start_x = -event.position.y
         progress_start = stick.progress_ratio
